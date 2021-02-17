f6e5884fd4dfd049bc03de3772fb2ae24aea102f
krhodes9 <61336730+krhodes9@users.noreply.github.com>
2021-02-16 19:18:59 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,45 @@
-require(["esri/Map", "esri/views/MapView"], function(Map, MapView) {
+require([
+        "esri/Map",
+        "esri/views/MapView",
+        "esri/layers/ImageryLayer",
+        "esri/layers/support/RasterFunction"
+      ], function(Map, MapView, ImageryLayer, RasterFunction) {
+
+
+        var imagePopupTemplate = {
+          // autocasts as new PopupTemplate()
+          title: "Data from Landsat 8 satellite",
+          content:
+            "Rendered RGB values: <b>{Raster.ServicePixelValue} </b>" +
+            "<br>Original values (CA, B, G, R, NIR, SWIR 1, SWIR 2, Pan, Cirrus, TIRS 1, TIRS 2): <b>{Raster.ItemPixelValue} </b>"
+        };
+
+var serviceRFT = new RasterFunction({
+          functionName: "NDVI",
+          rasterFunctionArguments: {
+           "VisibleBandID": 4,
+           "InfraredBandID": 5,},
+          variableName: "Raster"
+        });
+
+        var layer = new ImageryLayer({
+          url: "https://landsat2.arcgis.com/arcgis/rest/services/Landsat8_Views/ImageServer",
+          renderingRule: serviceRFT,
+          popupTemplate: imagePopupTemplate
+        });
+
         var map = new Map({
-          basemap: "dark-gray"
+          basemap: "hybrid",
+          layers: [layer]
         });
 
         var view = new MapView({
           container: "viewDiv",
           map: map,
-          zoom: 10,
-          center: [-90, 38] // longitude, latitude
+          center: [-90.1994, 38.6270],
+          zoom: 6,
+          popup: {
+            actions: []
+          }
         });
       });
